6e33c6b89358a34786b5568fddc5f133d861819f
Ryan Newton <rrnewton@gmail.com>
2024-01-13 17:20:28 -0500

    Add dependency on pleco

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use pleco::{Board,Player,PieceType};
+
 fn main() {
     println!("Hello, world!");
+
+    let board = Board::start_pos();
+    assert_eq!(board.count_piece(Player::White,PieceType::P), 8);
+    assert_eq!(&board.fen(),"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    println!("Done.");
 }
